120d680adfba65c090bd7bcaf4b42b3f071e1f54
Andreas Schantl <andreas.schantl@gmail.com>
2020-06-05 07:42:26 +0200

    Add version number to login screen

---
+++ config/expensemanager.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | ÖEINS Delivery Settings
+    |--------------------------------------------------------------------------
+    |
+    | These options determine the version of ÖEINS Delivery
+    |
+    */
+    'version' => 'v1.1.3' . (env('APP_ENV') === 'production' ? '' : '-Beta'),
+
+];
